900445665d9cc761667cf48e3fe4c0a88e692a7f
gmarcha <gmarcha@github.com>
2022-09-07 13:35:28 +0200

    Add warning to environment variable

--- internal/v1/poll/init.go
+++ internal/v1/poll/init.go
@@ -1,9 +1,31 @@
 package poll
 
-import "time"
+import (
+	"os"
+	"time"
+
+	"github.com/gmarcha/notion-trigger/internal/v1/log"
+	"github.com/jomei/notionapi"
+)
 
 var LastPollTime time.Time
 
 func init() {
 	LastPollTime = time.Now().Add(-time.Minute)
+}
+
+var (
+	campusDbId notionapi.DatabaseID
+	eventDbId  notionapi.DatabaseID
+)
+
+func init() {
+	campusDbId = notionapi.DatabaseID(os.Getenv("NOTION_CAMPUS_DB_ID"))
+	if campusDbId == "" {
+		log.Logger.Warn("Environment variable NOTION_CAMPUS_DB_ID is empty")
+	}
+	eventDbId = notionapi.DatabaseID(os.Getenv("NOTION_EVENT_DB_ID"))
+	if eventDbId == "" {
+		log.Logger.Warn("Environment variable NOTION_EVENT_DB_ID is empty")
+	}
 }
