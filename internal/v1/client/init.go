18166044e40505b488361ad59df5a46a3d240367
gmarcha <gmarcha@github.com>
2022-09-07 13:35:20 +0200

    Add warning to environment variable

--- internal/v1/client/init.go
+++ internal/v1/client/init.go
@@ -2,10 +2,15 @@ package client
 
 import (
 	"os"
+
+	"github.com/gmarcha/notion-trigger/internal/v1/log"
 )
 
 var notionToken string
 
 func init() {
 	notionToken = os.Getenv("NOTION_TOKEN")
+	if notionToken == "" {
+		log.Logger.Warn("Environment variable NOTION_TOKEN is empty")
+	}
 }
