e53bd38931b7acaf9248bb00a6e4dd325ffd926e
gmarcha <gmarcha@github.com>
2022-09-07 13:36:09 +0200

    Add errgroup and logging
    Add errgroup usage to perform multiple api requests in a for loop.
    If a request failed, errgroup stops others launched goroutines.
    
    Add error and debug logging.

--- internal/v1/poll/campus.go
+++ internal/v1/poll/campus.go
@@ -2,36 +2,38 @@ package poll
 
 import (
 	"context"
-	"log"
-	"os"
 
 	"github.com/gmarcha/notion-trigger/internal/v1/goswagger/client"
 	"github.com/gmarcha/notion-trigger/internal/v1/goswagger/client/task"
+	"github.com/gmarcha/notion-trigger/internal/v1/log"
 	"github.com/jomei/notionapi"
+	"golang.org/x/sync/errgroup"
 )
-
-var (
-	campusDbId notionapi.DatabaseID
-)
-
-func init() {
-	campusDbId = notionapi.DatabaseID(os.Getenv("NOTION_CAMPUS_DB_ID"))
-}
 
 func Campus(notion *notionapi.Client, api *client.Notion) error {
 
 	ctx := context.Background()
 	resp, err := notion.Database.Query(ctx, campusDbId, nil)
 	if err != nil {
+		log.Logger.Error("Notion API campus database query")
 		return err
 	}
+	log.Logger.Debug("Notion API campus database query")
+
+	var eg *errgroup.Group
 	for _, page := range resp.Results {
-		if page.CreatedTime.After(LastPollTime) {
-			_, err := api.Task.CampusCreate(&task.CampusCreateParams{ID: page.ID.String(), Context: ctx})
-			if err != nil {
-				log.Println(err)
+		currentPage := page
+		eg.Go(func() error {
+			if currentPage.CreatedTime.After(LastPollTime) {
+				_, err := api.Task.CampusCreate(&task.CampusCreateParams{ID: currentPage.ID.String(), Context: ctx})
+				if err != nil {
+					log.Logger.Error("Internal API create campus tasks")
+					return err
+				}
+				log.Logger.Debug("Internal API create campus tasks")
 			}
-		}
+			return nil
+		})
 	}
-	return nil
+	return eg.Wait()
 }
--- internal/v1/poll/pool.go
+++ internal/v1/poll/pool.go
@@ -2,21 +2,13 @@ package poll
 
 import (
 	"context"
-	"log"
-	"os"
 
 	"github.com/gmarcha/notion-trigger/internal/v1/goswagger/client"
 	"github.com/gmarcha/notion-trigger/internal/v1/goswagger/client/task"
+	"github.com/gmarcha/notion-trigger/internal/v1/log"
 	"github.com/jomei/notionapi"
+	"golang.org/x/sync/errgroup"
 )
-
-var (
-	eventDbId notionapi.DatabaseID
-)
-
-func init() {
-	eventDbId = notionapi.DatabaseID(os.Getenv("NOTION_EVENT_DB_ID"))
-}
 
 func Pool(notion *notionapi.Client, api *client.Notion) error {
 
@@ -28,15 +20,25 @@ func Pool(notion *notionapi.Client, api *client.Notion) error {
 	}
 	resp, err := notion.Database.Query(ctx, eventDbId, body)
 	if err != nil {
+		log.Logger.Error("Notion API event database query")
 		return err
 	}
+	log.Logger.Debug("Notion API event database query")
+
+	var eg *errgroup.Group
 	for _, page := range resp.Results {
-		if page.CreatedTime.After(LastPollTime) {
-			_, err := api.Task.PoolCreate(&task.PoolCreateParams{ID: page.ID.String(), Context: ctx})
-			if err != nil {
-				log.Println(err)
+		currentPage := page
+		eg.Go(func() error {
+			if currentPage.CreatedTime.After(LastPollTime) {
+				_, err := api.Task.PoolCreate(&task.PoolCreateParams{ID: currentPage.ID.String(), Context: ctx})
+				if err != nil {
+					log.Logger.Error("Internal API create pool tasks")
+					return err
+				}
+				log.Logger.Debug("Internal API create pool tasks")
 			}
-		}
+			return nil
+		})
 	}
-	return nil
+	return eg.Wait()
 }
